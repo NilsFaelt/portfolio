fdf6262cf4de82f123718de4bff09b2b85e4530a
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-24 08:35:12 +0100

    lighthouse

--- components/SvgLinkBar/SvgLinkBar.component.tsx
+++ components/SvgLinkBar/SvgLinkBar.component.tsx
@@ -5,16 +5,25 @@ export const SvgLinkBar: FC = () => {
   return (
     <Container>
       <StyledHref
+        aria-label='link to linkedin'
         target='_blank'
         href='https://www.linkedin.com/in/nils-faelt-73b163227/'
       >
-        <StyledSvg src='/svg/linkedin.svg' />
+        <StyledSvg src='/svg/linkedin.svg' alt='linkedin logo' />
       </StyledHref>
-      <StyledHref target='_blank' href='https://github.com/NilsFaelt'>
-        <StyledSvg src='/svg/github.svg' />
+      <StyledHref
+        aria-label='link to github'
+        target='_blank'
+        href='https://github.com/NilsFaelt'
+      >
+        <StyledSvg alt='github logo' src='/svg/github.svg' />
       </StyledHref>
-      <StyledHref target='_blank' href='mailto:nilsfalt@gmail.com'>
-        <StyledSvg src='/svg/email.svg' />
+      <StyledHref
+        aria-label='link to gmail'
+        target='_blank'
+        href='mailto:nilsfalt@gmail.com'
+      >
+        <StyledSvg alt='email logo' src='/svg/email.svg' />
       </StyledHref>
     </Container>
   );
