fb59b73ac546e0e40a1b26d8f7099c1fe5476ab4
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-03-27 11:27:55 +0100

    rewind

--- components/Section/Section.style.tsx
+++ components/Section/Section.style.tsx
@@ -1,15 +1,8 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-export const Container = styled.div<{ isVisible: boolean }>`
-  opacity: 0;
-  transform: translateY(20px);
-  transition: opacity 3.5s ease, transform 3.5s ease;
-
-  /* Apply styles when section is visible */
-  ${(props) =>
-    props.isVisible &&
-    css`
-      opacity: 1;
-      transform: translateY(0);
-    `}
+export const Container = styled.div`
+  display: flex;
+  background-color: black;
+  width: 100vw;
+  min-height: 100vh;
 `;
