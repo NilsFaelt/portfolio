86e5d188dcd3b4176b1eb67e16a6e91f8424b5f7
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-03-27 11:14:35 +0100

    testing new animatiom

--- components/Section/Section.component.tsx
+++ components/Section/Section.component.tsx
@@ -1,9 +1,38 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useRef, useEffect, useState } from "react";
 import { Container } from "./Section.style";
+
 interface Props {
   children: ReactNode;
 }
 
 export const Section: FC<Props> = ({ children }) => {
-  return <Container>{children}</Container>;
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target); // Stop observing once section is visible
+        }
+      });
+    });
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <Container ref={sectionRef} isVisible={isVisible}>
+      {children}
+    </Container>
+  );
 };
