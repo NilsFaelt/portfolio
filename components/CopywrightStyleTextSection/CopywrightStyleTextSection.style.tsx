e9f2e5a39bbc857aecc8b1187d76e636e1e7b0bc
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-04-15 09:45:48 +0200

    added margin magifying glass

--- components/CopywrightStyleTextSection/CopywrightStyleTextSection.style.tsx
+++ components/CopywrightStyleTextSection/CopywrightStyleTextSection.style.tsx
@@ -27,6 +27,9 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   max-width: 20rem;
+  @media (max-width: 597px) {
+    margin-left: 1rem;
+  }
 `;
 
 export const Text = styled.p`
